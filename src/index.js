05a38160cb480a101b04a93fc9fad708d2da7137
Ben Orozco <benoror@gmail.com>
2018-07-11 16:25:24 -0500

    Chore: Add & Configure Firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAmpTryHcBD88c9FUkBdm3IUbNFEVDSbNA",
+  authDomain: "hackweek2018-instagram-clone.firebaseapp.com",
+  databaseURL: "https://hackweek2018-instagram-clone.firebaseio.com",
+  projectId: "hackweek2018-instagram-clone",
+  storageBucket: "hackweek2018-instagram-clone.appspot.com",
+  messagingSenderId: "933763647325"
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
