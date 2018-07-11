cd18448478ce991560d7c5ce0ecb3dc55e9b0f12
Ben Orozco <benoror@gmail.com>
2018-07-11 16:50:07 -0500

    Feat[auth]: Display user name

--- src/App.js
+++ src/App.js
@@ -4,13 +4,41 @@ import './App.css';
 import firebase from 'firebase';
 
 class App extends Component {
-  handleAuth() {
-    const provider = new firebase.auth.GoogleAuthProvider();
+  constructor() {
+    super();
+    this.state = {
+      user: null
+    }
+  }
+
+  componentWillMount() {
+    firebase.auth().onAuthStateChanged(user => this.setState({ user }))
+  }
+
+  handleAuth = () => {
+    const provider = new firebase.auth.GoogleAuthProvider()
 
     firebase.auth().signInWithPopup(provider)
       .then(result => console.log(`${result.user.email} ha iniciado sesion`))
       .catch(error => console.log(`Error ${error.code}: ${error.message} `))
   }
+
+  renderLoginButton = () => {
+    if (!this.state.user) {
+      return (
+        <button onClick={this.handleAuth} className="app-btn">
+          Iniciar Sesión
+        </button>
+      )
+    } else {
+      return (
+        <div>
+          Hola {this.state.user.displayName}
+        </div>
+      )
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -18,11 +46,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          <button onClick={this.handleAuth} className="App-btn">
-            Iniciar sesión
-          </button>
-        </p>
+        {this.renderLoginButton()}
       </div>
     );
   }
