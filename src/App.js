a8dd64b29f95058f85264390128f1a5fe7a40380
Ben Orozco <benoror@gmail.com>
2018-07-11 16:36:44 -0500

    Feat[firebase]: Ass basic Auth

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase';
 
 class App extends Component {
+  handleAuth() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider)
+      .then(result => console.log(`${result.user.email} ha iniciado sesion`))
+      .catch(error => console.log(`Error ${error.code}: ${error.message} `))
+  }
   render() {
     return (
       <div className="App">
@@ -11,7 +19,9 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <button onClick={this.handleAuth} className="App-btn">
+            Iniciar sesión
+          </button>
         </p>
       </div>
     );
